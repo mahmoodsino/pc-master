d6350399116fb4bfda3c067db7b4a9622905c04e
mahmood2122 <mahmoodsino40@gmail.com>
2022-09-21 12:10:19 +0300

    fix

--- helper/sever/products/featuredProducts/services.ts
+++ helper/sever/products/featuredProducts/services.ts
@@ -9,7 +9,7 @@ const getfeaturedProducts = async (token: string, id?: number) => {
         if (id) {
             const res = await axios.get(`${root}/products?page_size=5&is_featured=1&category=${id}`, {
                 headers: {
-                    "branch_id": 1,
+                    "branch-id": 1,
                     "company-id": 1,
                     Authorization: `Bearer ${token}`
                 }
@@ -19,7 +19,7 @@ const getfeaturedProducts = async (token: string, id?: number) => {
 
             const res = await axios.get(`${root}/products?page_size=5&is_featured=1`, {
                 headers: {
-                    "branch_id": 1,
+                    "branch-id": 1,
                     "company-id": 1,
                     Authorization: `Bearer ${token}`
                 }
